04bcc2417d24b6b072d21d7fd7f0697373177366
LeeWooJung <wjlee612@gmail.com>
2024-08-18 00:58:00 +0900

    Naver API Credential getter

---
+++ 6. Sourcecode/etl/src/main/java/extract/crawling/credentialLoader.java
@@ -0,0 +1,37 @@
+package extract.crawling;
+
+import java.io.*;
+
+public class credentialLoader {
+    private String clientId;
+    private String clientSecret;
+
+    public credentialLoader(String filePath) {
+        loadCredentials(filePath);
+    }
+
+    private void loadCredentials(String filePath) {
+
+        try(InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(filePath);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+
+                if(inputStream == null) {
+                    throw new IllegalArgumentException("There is no file in " + filePath);
+                }
+
+                clientId = reader.readLine().split("=")[1].trim();
+                clientSecret = reader.readLine().split("=")[1].trim();
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+}
